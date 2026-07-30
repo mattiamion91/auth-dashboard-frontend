30ac99118465c2d1bcdab99cb6f3e50226b074e0
mattiamion91 <mattia.mion@gmail.com>
2026-07-30 16:32:49 +0200

    feat: Dashboard.jsx done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,23 @@
+import { useAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 function Dashboard() {
-    return <div>Dashboard - da costruire</div>;
+    const { user, sendLogout } = useAuth();
+    const navigate = useNavigate();
+
+    async function logoutChangingPath () {
+        await sendLogout()
+        navigate('../login')
+    }
+
+    return (
+        <>
+            <p>Benvenuto {user.email}</p>
+            <p>il tuo ruolo é {user.role}</p>
+            {user.role === 'admin' && <p>sei un amministratore!!</p>}
+            <button onClick={logoutChangingPath}>Logout</button>
+        </>
+    )
 }
 
 export default Dashboard;
